e57a87bdac33598437365ab1927ad600b791511b
ottnicoly <nicolyott23@gmail.com>
2025-05-04 18:04:04 -0300

    feat: first commit

---
+++ src/main/java/com/jogajunto/JogaJunto/JogaJuntoApplication.java
@@ -0,0 +1,13 @@
+package com.jogajunto.JogaJunto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogaJuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogaJuntoApplication.class, args);
+	}
+
+}
